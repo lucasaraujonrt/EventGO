92b2feca8e86539a0c043c84a983dd9d4cb99f6b
LucasA <37451939+lucasaraujonrt@users.noreply.github.com>
2020-05-24 13:23:09 -0300

    Create routes

--- Mobile EventGO/src/pages/Home/index.js
+++ Mobile EventGO/src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import react from "react";
 import { View } from "react-native";
 
-export default function ViewEvent() {
+export default function Home() {
   return <View />;
 }
--- Mobile EventGO/src/routes.js
+++ Mobile EventGO/src/routes.js
@@ -1,1 +1,22 @@
-import react from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const appStack = crecreateStackNavigator();
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Event from "./pages/Event";
+import Ticket from "./pages/Ticket";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <appStack.Navigator>
+        <appStack.Screen component={Home} />
+        <appStack.Screen component={Login} />
+        <appStack.Screen component={Event} />
+        <appStack.Screen component={Ticket} />
+      </appStack.Navigator>
+    </NavigationContainer>
+  );
+}
