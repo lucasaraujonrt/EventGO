0fe84d12c8fb7c4d1242a7bc41cee1cd318bf9e6
LucasA <37451939+lucasaraujonrt@users.noreply.github.com>
2020-05-25 11:57:26 -0300

    Ticket Done

--- Mobile EventGO/src/pages/Ticket/index.js
+++ Mobile EventGO/src/pages/Ticket/index.js
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
+import logoImg from "../../assets/logo/logo.png";
+import styles from "./style";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Ticket() {
+  const navigation = useNavigation();
+
+  function navigationToHome() {
+    navigation.navigate("Home");
+  }
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <TouchableOpacity onPress={navigationToHome}>
+          <Image source={logoImg} />
+        </TouchableOpacity>
+        <View style={styles.textConfirm}>
+          <Text style={styles.text}>Participação confirmada!!</Text>
+        </View>
+        <View style={styles.textId}>
+          <Text style={styles.text}>O ID do seu ingresso é:</Text>
+        </View>
+      </View>
     </View>
   );
 }
---
+++ Mobile EventGO/src/pages/Ticket/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#285EC4",
+    flex: 1,
+  },
+  text: {
+    fontSize: 30,
+    marginTop: 100,
+  },
+  textConfirm: {},
+});
