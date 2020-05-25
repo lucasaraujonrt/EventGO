58daf19851f3d9e59e7cfbb3bc571ca9b9f9f1ae
LucasA <37451939+lucasaraujonrt@users.noreply.github.com>
2020-05-25 11:40:17 -0300

    Login Screen done

--- Mobile EventGO/src/pages/Login/index.js
+++ Mobile EventGO/src/pages/Login/index.js
@@ -1,6 +1,59 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import logoImg from "../../assets/logo/logo.png";
+import styles from "./style";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login() {
-  return <View />;
+  const navigation = useNavigation();
+
+  function navigationToTicket() {
+    navigation.navigate("Ticket");
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+      </View>
+      <View style={styles.login}>
+        <TextInput
+          placeholder={"E-mail"}
+          textContentType={"emailAddress"}
+          style={styles.loginEmail}
+        >
+          <AntDesign name="user" size={24} color="#FFFAFA" />
+        </TextInput>
+      </View>
+      <View style={styles.login}>
+        <TextInput
+          placeholder={"Password"}
+          textContentType={"password"}
+          style={styles.loginPassword}
+        >
+          <AntDesign name="lock" size={24} color="#FFFAFA" />
+        </TextInput>
+      </View>
+      <View style={{ backgroundColor: "red" }}>
+        <TouchableOpacity style={styles.button} onPress={navigationToTicket}>
+          <Text style={styles.textButton}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.loginGoogle}>
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.iconGoogle}>
+            <AntDesign name="google" size={24} color="#FFFAFA" />
+          </View>
+          <View style={styles.textSignIn}>
+            <Text style={styles.textGoogle}>Sign In With Google</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.forgotPassoword}>
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.forgotText}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
---
+++ Mobile EventGO/src/pages/Login/style.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "#285EC4",
+    flex: 1,
+    flexDirection: "column",
+  },
+  login: {
+    backgroundColor: "red",
+  },
+  loginEmail: {
+    backgroundColor: "#2E323E",
+    width: 300,
+    height: 40,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  loginPassword: {
+    backgroundColor: "#2E323E",
+    width: 300,
+    height: 40,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#2E323E",
+    borderRadius: 10,
+    width: 100,
+    height: 30,
+    justifyContent: "center",
+    marginTop: 100,
+  },
+  textButton: {
+    color: "#FFFAFA",
+  },
+  textGoogle: {
+    color: "#FFFAFA",
+  },
+});
